fe7c555ed9add5e817b8f5e5e3db429c8ec38ed9
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-03-16 17:22:59 +0100

    working radix

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: arabenst <arabenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 16:34:17 by arabenst          #+#    #+#             */
-/*   Updated: 2023/03/15 13:49:30 by arabenst         ###   ########.fr       */
+/*   Updated: 2023/03/16 17:09:00 by arabenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,19 @@ int	ft_is_sorted(t_stack *stack)
 			> stack->stack[(stack->top + i + 1) % stack->count])
 			return (0);
 	return (1);
+}
+
+void	ft_sort_three(t_push_swap *data)
+{
+	if (data->a->stack[data->a->top] == 2)
+		ft_execute_op(data, ROT | A);
+	else if (data->a->stack[data->a->top] == 1
+		&& data->a->stack[(data->a->top + 1) % data->a->count] == 2)
+		ft_execute_op(data, REV | A);
+	else
+		ft_execute_op(data, SWAP | A);
+	if (!ft_is_sorted(data->a))
+		ft_sort_three(data);
 }
 
 int	ft_get_index_of_lowest_number(t_stack *stack)
@@ -77,9 +90,28 @@ void	ft_selection_sort(t_push_swap *data)
 		ft_execute_op(data, PUSH | A);
 }
 
-void	ft_sort_three(t_push_swap *data)
+void	ft_radix_sort(t_push_swap *data)
 {
-	
+	int	size;
+	int	i;
+	int	bit;
+
+	size = data->a->count;
+	bit = 0;
+	while (!(ft_is_sorted(data->a)))
+	{
+		i = -1;
+		while (++i < size)
+		{
+			if ((data->a->stack[data->a->top] >> bit) & 1)
+				ft_execute_op(data, ROT | A);
+			else
+				ft_execute_op(data, PUSH | B);
+		}
+		while (data->b->count)
+			ft_execute_op(data, PUSH | A);
+		bit++;
+	}
 }
 
 void	ft_sort_into_queue(t_push_swap *data)
@@ -91,5 +123,6 @@ void	ft_sort_into_queue(t_push_swap *data)
 	else if (data->a->count < 10)
 		ft_selection_sort(data);
 	else
-		ft_selection_sort(data);
+		ft_radix_sort(data);
+	// print_stack(data->a);
 }
